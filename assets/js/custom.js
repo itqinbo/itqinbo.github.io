eeead3ae7047fd889fc284c9fe6f071143c8ad04
itqinbo <itqinbo@163.com>
2019-09-06 19:21:18 +0800

    初始化

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+﻿
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        animation_fun: function () {
+
+            /*====================================
+               WOW ANIMATION SCRIPTS 
+              ======================================*/
+            new WOW().init();
+
+        },
+       
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.animation_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+
